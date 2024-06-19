22249b3d10f29e0a8cb6575dc5d6bac4bf79fd56
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2024-06-20 01:03:49 +0200

    Task requirements

--- Zadanie1/Osoba.cs
+++ Zadanie1/Osoba.cs
@@ -2,33 +2,77 @@
 
 public class Osoba
 {
-    private string _imie;
-    private string _nazwisko;
-    private DateTime DataUrodzin;
-    private DateTime DataSmierci;
-    private string imieNazwisko;
-    private int wiek;
+    private string imie;
+    private string nazwisko;
+    private DateTime? DataUrodzin { get; set; }
+    private DateTime? DataSmierci { get; set; }
 
     public Osoba(string imieNazwisko)
     {
-        this.imieNazwisko = imieNazwisko;
+       ImieNazwisko = imieNazwisko;
     }
 
     public string Imie
     {
-        get { return _imie; }
-        set { }
+        get { return imie; }
+        set
+        {
+
+            if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
+            {
+                imie = value.Trim();
+            }
+            else
+            {
+                throw new ArgumentException("Imie nie może być puste!!!");
+            }
+        }
 
     }
 
     public string ImieNazwisko
     {
-        get { return null;}
-        set{ }
+        get { return $"{imie} {nazwisko}"; }
+        set
+        {
+            string[] personName = value.Split(' ');
+            if (personName.Length == 2)
+            {
+                imie = personName[0];
+                nazwisko = personName[1];
+            }
+            else if (personName.Length == 1)
+            {
+                imie = personName[0];
+                nazwisko = String.Empty;
+            }
+            else
+            {
+                throw new ArgumentException("Podaj imię i nazwisko");
+            }
+        }
     }
 
-    public int Wiek
+    public TimeSpan? Wiek
     {
-        get { return wiek; }
+        get
+        {
+            if (DataUrodzin.HasValue )
+            {            
+                if(DataSmierci.HasValue)
+                {
+                    return DataSmierci.Value - DataUrodzin.Value;
+                }
+                else
+                {
+                    return DateTime.Now - DataUrodzin.Value;
+                }
+                
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
