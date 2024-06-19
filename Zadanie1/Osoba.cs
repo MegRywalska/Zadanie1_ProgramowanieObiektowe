45e639ba0d0bd6038c31b89a5732ee1b01a5d3c7
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2024-06-19 23:57:15 +0200

    Created the Person class template

---
+++ Zadanie1/Osoba.cs
@@ -0,0 +1,34 @@
+﻿namespace Zadanie1;
+
+public class Osoba
+{
+    private string _imie;
+    private string _nazwisko;
+    private DateTime DataUrodzin;
+    private DateTime DataSmierci;
+    private string imieNazwisko;
+    private int wiek;
+
+    public Osoba(string imieNazwisko)
+    {
+        this.imieNazwisko = imieNazwisko;
+    }
+
+    public string Imie
+    {
+        get { return _imie; }
+        set { }
+
+    }
+
+    public string ImieNazwisko
+    {
+        get { return null;}
+        set{ }
+    }
+
+    public int Wiek
+    {
+        get { return wiek; }
+    }
+}
