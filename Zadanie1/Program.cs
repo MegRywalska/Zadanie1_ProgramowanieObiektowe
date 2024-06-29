b996222ee7dcad64696d073a7ce6cfb60193ef17
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2024-06-29 15:23:14 +0200

    Adding the main method

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,3 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba1 = new Osoba("Jan Kowalski");
+            osoba1.DataUrodzin = new DateTime(1990, 1, 1);
+
+            Console.WriteLine("Osoba 1:");
+            Console.WriteLine($"Imię i nazwisko: {osoba1.ImieNazwisko}");
+            Console.WriteLine($"Data urodzin: {osoba1.DataUrodzin}");
+            Console.WriteLine($"Wiek: {osoba1.Wiek?.Days / 365.25} lat");
+            
+            osoba1.ImieNazwisko = "Adam Nowak";
+            Console.WriteLine("\nZmiana danych osoby 1:");
+            Console.WriteLine($"Imię i nazwisko: {osoba1.ImieNazwisko}");
+            
+            Osoba osoba2 = new Osoba("Anna");
+            Console.WriteLine("\nOsoba 2:");
+            Console.WriteLine($"Imię i nazwisko: {osoba2.ImieNazwisko}");
+            
+            try
+            {
+                osoba2.Imie = "";
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"\nBłąd: {ex.Message}");
+            }
+
+            
+            osoba1.DataSmierci = new DateTime(2020, 1, 1);
+            Console.WriteLine("\nPo ustawieniu daty śmierci osoby 1:");
+            Console.WriteLine($"Data śmierci: {osoba1.DataSmierci}");
+            Console.WriteLine($"Wiek: {osoba1.Wiek?.Days / 365.25} lat"); 
+            
+        }
+    }
+}
